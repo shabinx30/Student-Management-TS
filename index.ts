c5cf5b17449daa5d39da153faf6e4db69180d780
shabinx30 <shabeensharih@gmail.com>
2024-11-18 12:44:20 +0530

    server setup and added basic packages

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
 import dotenv from 'dotenv'
+import path from 'path'
+import bodyParser, { urlencoded } from 'body-parser'
+import express, { Request, Response } from 'express'
+
+const app = express()
+dotenv.config()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.listen(3000,()=>{
+    console.log('started');
+})
